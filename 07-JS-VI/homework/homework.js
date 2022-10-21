c04a97390e0ab38e56b4f2eefeece044afb3af8d
nicolasariel98 <nicolasarielvelazquez1998@gmail.com>
2022-10-21 16:09:15 -0300

    terminado

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -46,7 +46,7 @@ function map(array, cb) {
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
   var nuevoArray = array.map(function(elemento){
-  return cb(elemento);
+   return cb(elemento);
   });
   return nuevoArray;
 }
